4a705fdc9b6a62e7a8f66fb4f015df466bf9fca5
Anton <medvedev.workmail@gmail.com>
2026-01-31 15:16:21 +0200

    removed env files from repo

--- src/config/validation.schema.ts
+++ src/config/validation.schema.ts
@@ -2,7 +2,7 @@ import * as Joi from 'joi';
 
 const validationSchema = Joi.object({
   NODE_ENV: Joi.string()
-    .valid('development', 'local', 'production')
+    .valid('development', 'example', 'local', 'production')
     .default('development'),
   PORT: Joi.number().default(3000),
 });
